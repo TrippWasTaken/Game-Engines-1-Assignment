1f54370c8f6f046bce4abf91f36ac6e1801fab4c
Konrad Knecht <konrad.knecht@gmail.com>
2019-12-15 01:59:52 +0000

    replaced simple mesh generation with inifnite terrain generation and added skybox

---
+++ GE Assignment/Assets/GenerateInfinite.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Tile
+{
+    public GameObject theTile;
+    public float creationTime;
+
+    public Tile(GameObject tile, float time)
+    {
+        theTile = tile;
+        creationTime = time;
+    }
+}
+public class GenerateInfinite : MonoBehaviour
+{
+
+    public GameObject plane;
+    public GameObject cam;
+
+    int planeSize = 10;
+    int halfTilesX = 5;
+    int halfTilesZ = 10;
+
+    Vector3 startPos;
+    Hashtable tiles = new Hashtable();
+
+    void Start()
+    {
+        this.gameObject.transform.position = Vector3.zero;
+        startPos = Vector3.zero;
+
+        float updateTime = Time.realtimeSinceStartup;
+
+        for(int x = -halfTilesX; x < halfTilesX; x++)
+        {
+            for(int z = -halfTilesZ; z < halfTilesZ; z++)
+            {
+                Vector3 pos = new Vector3((x * planeSize + startPos.x), 0, (z * planeSize + startPos.z));
+                GameObject tileN = (GameObject)Instantiate(plane, pos, Quaternion.identity);
+
+                string tilename = "Tile_" + ((int)(pos.x)).ToString() + "_" + ((int)(pos.z)).ToString();
+                tileN.name = tilename;
+                Tile tile = new Tile(tileN, updateTime);
+                tiles.Add(tilename, tile);
+            }
+        }
+        
+    }
+
+    void Update()
+    {
+        int zMove = (int)(cam.transform.position.z - startPos.z);
+
+        if(Mathf.Abs(zMove) >= planeSize)
+        {
+            float updateTime = Time.realtimeSinceStartup;
+
+            int camZ = (int)(Mathf.Floor(cam.transform.position.z / planeSize) * planeSize);
+
+            for (int x = -halfTilesX; x < halfTilesX; x++)
+            {
+                for (int z = -halfTilesZ; z < halfTilesZ; z++)
+                {
+                    Vector3 pos = new Vector3((x * planeSize + startPos.x), 0, (z * planeSize + startPos.z));
+
+                    string tilename = "Tile_" + ((int)(pos.x)).ToString() + "_" + ((int)(pos.z)).ToString();
+
+                    if (!tiles.ContainsKey(tilename))
+                    {
+                        GameObject tileN = (GameObject)Instantiate(plane, pos, Quaternion.identity);
+
+                        tileN.name = tilename;
+                        Tile tile = new Tile(tileN, updateTime);
+                        tiles.Add(tilename, tile);
+                    }
+                    else
+                    {
+                        (tiles[tilename] as Tile).creationTime = updateTime;
+                    }
+                   
+                }
+            }
+
+            Hashtable newTerrain = new Hashtable();
+            foreach (Tile t in tiles.Values)
+            {
+                if (t.creationTime != updateTime)
+                {
+                    Destroy(t.theTile);
+                }
+                else
+                {
+                    newTerrain.Add(t.theTile.name, t);
+                }
+            }
+            tiles = newTerrain;
+
+            startPos = cam.transform.position;
+        }
+    }
+}
---
+++ GE Assignment/Assets/GenerateTerrain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTerrain : MonoBehaviour
+{
+    int heightScale = 5;
+    float detailScale = 5.0f;
+
+    private void Start()
+    {
+        Mesh mesh = this.GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i].y = Mathf.PerlinNoise((vertices[i].x + this.transform.position.x) / detailScale, (vertices[i].z + this.transform.position.z) / detailScale) * heightScale;
+        }
+
+        mesh.vertices = vertices;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        this.gameObject.AddComponent<MeshCollider>();
+    }
+}
+
